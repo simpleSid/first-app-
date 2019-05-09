7ed382c0e331a69632183593a476723548b08f94
Денис Сидоренко <itklass@mail.ru>
2019-05-09 21:26:07 +0300

    lesson 9: add new storybord, add label and button, and button action to change the label text

--- autoLayout/ViewController.swift
+++ autoLayout/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func changeTextInLabel(_ sender: UIButton) {
+        label.text = "Hello, World!"
+    }
+    
 }
 
