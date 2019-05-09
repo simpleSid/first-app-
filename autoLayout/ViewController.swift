d37626d5fc75091ac9b06c4beb5d6391c23d9764
Денис Сидоренко <itklass@mail.ru>
2019-05-09 17:59:23 +0300

    initital commit

---
+++ autoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Денис Сидоренко on 09/05/2019.
+//  Copyright © 2019 Денис Сидоренко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
