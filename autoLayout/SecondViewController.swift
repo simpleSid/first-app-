fba997c75b88fec51c6d370264d133a718cd79b3
Денис Сидоренко <itklass@mail.ru>
2019-05-16 16:13:59 +0300

    17 lesson: start new project PassDataProject. Add new storybord and 2 viewcontrollers.

---
+++ autoLayout/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  autoLayout
+//
+//  Created by Денис Сидоренко on 16/05/2019.
+//  Copyright © 2019 Денис Сидоренко. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+        
+    }
+}
---
+++ autoLayout/ViewController5.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController5.swift
+//  autoLayout
+//
+//  Created by Денис Сидоренко on 16/05/2019.
+//  Copyright © 2019 Денис Сидоренко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController5: UIViewController {
+
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func passData(_ sender: UIButton) {
+        
+    }
+    
+}
